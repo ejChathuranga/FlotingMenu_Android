02365f3e3a08d8e5c27b6922f284427a95f4733b
EJ Chathuranga <ejcindunil@gmail.com>
2017-11-10 12:20:14 +0530

    added floating menu and buttons

--- app/src/main/java/com/android/ejsoft/flotingmenu/MainActivity.java
+++ app/src/main/java/com/android/ejsoft/flotingmenu/MainActivity.java
@@ -1,13 +1,51 @@
 package com.android.ejsoft.flotingmenu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    FloatingActionMenu materialDesignFAM;
+    FloatingActionButton floatingActionButton1, floatingActionButton2, floatingActionButton3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        materialDesignFAM = (FloatingActionMenu) findViewById(R.id.material_design_android_floating_action_menu);
+        floatingActionButton1 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item1);
+        floatingActionButton2 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item2);
+        floatingActionButton3 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item3);
+
+        floatingActionButton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu first item clicked
+                Toast.makeText(MainActivity.this, "floatingActionButton1", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu second item clicked
+                Toast.makeText(MainActivity.this, "floatingActionButton2", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        floatingActionButton3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu third item clicked
+                Toast.makeText(MainActivity.this, "floatingActionButton3", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
